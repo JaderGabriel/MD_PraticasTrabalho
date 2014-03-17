90229b3c384d7493cf07cde2fd635c11906e1b82
jadergabriel <programacaoeti@outlook.com>
2014-03-17 00:31:49 -0300

    Datas

--- mprojetct.py
+++ mprojetct.py
@@ -6,5 +6,5 @@ __license__ = "GPL 1"
 __email__ = "programacaoeti@outlook.com\n"
 __status__ = "\nTrabalho NRO DO TRABALHO\n"
 _data__ = '17/03/14'
-    
-print(__tags__)
+import datetime as time
+print("Agora:\n %s" %time.datetime.now())
