0831ed73a0663411e25fb1ff6544c906dccd09f9
jadergabriel <programacaoeti@outlook.com>
2014-03-17 00:25:38 -0300

    Seg

---
+++ mprojetct.py
@@ -0,0 +1,10 @@
+__author__ = "Jader Gabriel\n"
+__copyright__ = "\nCopyright 2013\n"
+__tags__ = ["Unimontes", "CCET", "DCC",
+            "NOME DA DISCIPLINA", "Prfº: NOME DO PROFESSOR"]
+__license__ = "GPL 1"
+__email__ = "programacaoeti@outlook.com\n"
+__status__ = "\nTrabalho NRO DO TRABALHO\n"
+_data__ = '17/03/14'
+    
+print(__tags__)
